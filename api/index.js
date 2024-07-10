dff5b9bffeca1866da58fe745a068ac68fba1d0f
Anirudh Sriram <srirama@cs.uchicago.edu>
2024-07-10 12:59:26 +0200

    vercel changes

--- api/index.js
+++ api/index.js
@@ -26,8 +26,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    credentials: true,
-    origin: "http://localhost:5173",
+    // credentials: true,
+    // origin: "http://localhost:5173",
   })
 );
 
